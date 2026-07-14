11ed745dc91561e58270c80f7e5b72d2d9f789b1
a8anassis <a8anassis@gmail.com>
2026-07-14 19:45:19 +0300

    feat(controller): update HelloController to support versioned greeting endpoints

--- src/main/java/gr/aueb/cf/starter_mavenpro/controller/HelloController.java
+++ src/main/java/gr/aueb/cf/starter_mavenpro/controller/HelloController.java
@@ -4,14 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/starter-maven")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/v1/hello")
     public String hello(Model model) {
         model.addAttribute("message", "Hello Maven!!!");
+        return "index";
+    }
+
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/v2/hello")
+    public String hello2(Model model) {
+        model.addAttribute("message", "Hello Coding!!!");
         return "index";
     }
 }
